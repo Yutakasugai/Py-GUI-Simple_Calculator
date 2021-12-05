a824f4e867756a66383bfa315a1ea38be3e23a25
Yutakasugai <90221229+Yutakasugai@users.noreply.github.com>
2021-12-05 01:14:55 -0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,145 @@
+from tkinter import *
+from typing import Match
+
+# Create an environemnt for application
+root = Tk()
+root.title("Simple Calculator")
+
+# Create an input label at the top
+e = Entry(root, width=30, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+# React for a button by clicking
+
+
+def button_click(number):
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
+
+# Delete all inserted numbers
+
+
+def button_clear():
+    e.delete(0, END)
+
+# Function to capture a first number for adding with a second number
+
+
+def button_add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+# Function to define which button user click to add, minus, multiply or divide
+
+
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))
+
+    if math == "subtraction":
+        e.insert(0, f_num - int(second_number))
+
+    if math == "multiplication":
+        e.insert(0, f_num * int(second_number))
+
+    if math == "division":
+        e.insert(0, f_num / int(second_number))
+
+# Function to capture a first number for subtracting
+
+
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+# Function to capture a first number for multiplying
+
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+# Function to capture a first number for dividing
+
+
+def button_divide():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+# define buttons
+button_1 = Button(root, text="1", padx=40, pady=20,
+                  command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20,
+                  command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20,
+                  command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20,
+                  command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20,
+                  command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20,
+                  command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20,
+                  command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20,
+                  command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20,
+                  command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20,
+                  command=lambda: button_click(0))
+button_plus = Button(root, text="+", padx=40, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=90, pady=20, command=button_equal)
+button_clear = Button(root, text="Clear", padx=80,
+                      pady=20, command=button_clear)
+
+button_subtract = Button(root, text="-", padx=40,
+                         pady=20, command=button_subtract)
+button_multiply = Button(root, text="*", padx=40,
+                         pady=20, command=button_multiply)
+button_divide = Button(root, text="/", padx=40, pady=20, command=button_divide)
+
+# Put the button on the screen
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_plus.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+
+# Important: apllication must be running into the loop as long as users are in this app
+root.mainloop()
